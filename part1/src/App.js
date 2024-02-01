883c8287412ea29090b7789ba17e2730f1d1f032
Jakub <jakramp96@gmail.com>
2024-02-01 23:47:08 +0100

    WIP 1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,4 +1,19 @@
-const Header = (props) => {
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+export default Appconst Header = (props) => {
   return (
    <h1>{props.course.name}</h1>
   )
